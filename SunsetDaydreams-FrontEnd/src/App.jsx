185ab1e193c902f810eccc54dab777159cfb9044
Mike-Testut <michael.testut@gmail.com>
2025-10-28 22:42:22 -0700

    update routes

--- SunsetDaydreams-FrontEnd/src/App.jsx
+++ SunsetDaydreams-FrontEnd/src/App.jsx
@@ -3,15 +3,17 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Cart from './pages/Cart'
 import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Orders" element={<Orders />} />
-        <Route path="/PlaceOrder" element={<PlaceOrder />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/placeOrder" element={<PlaceOrder />} />
+        <Route path="/products/:productID" element={<Product />} />
 
       </Routes>
 

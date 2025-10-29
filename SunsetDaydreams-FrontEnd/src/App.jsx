7c65ced4edb08a930f5976adcd5d8375aa4d6288
Mike-Testut <michael.testut@gmail.com>
2025-10-28 22:40:44 -0700

    start routes setup

--- SunsetDaydreams-FrontEnd/src/App.jsx
+++ SunsetDaydreams-FrontEnd/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Orders" element={<Orders />} />
+        <Route path="/PlaceOrder" element={<PlaceOrder />} />
 
+      </Routes>
 
     </div>
   )
